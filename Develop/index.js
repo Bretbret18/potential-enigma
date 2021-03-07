2276678e7af78438e8965f432b323f1328138080
Bretbret18 <bret.sperry@gmail.com>
2021-03-07 15:38:21 -0500

    finish

--- Develop/index.js
+++ Develop/index.js
@@ -9,7 +9,8 @@ const questions = [
         type: "input",
         name: "github",
         message: "What is your GitHub username?"
-      },{
+      },
+      {
         type: "input",
         name: "email",
         message: "What is your email address?"
